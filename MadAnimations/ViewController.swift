262c44188d34864dda7fa1097328b2d7ef64d631
Alexandr-Mayyura <sonickill@yandex.ru>
2022-10-31 22:36:17 +0300

    Initial Commit

---
+++ MadAnimations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MadAnimations
+//
+//  Created by Aleksandr Mayyura on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
